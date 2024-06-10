8e6eb6b00226037f6602ed1b0aaaef43848f1e18
Pavel Petkov <elias5247698@abv.bg>
2024-06-10 12:09:53 +0300

    making basic main for directory serilisation and fixing gitignore

--- FileSubsystem/main.cpp
+++ FileSubsystem/main.cpp
@@ -4,5 +4,9 @@
 int main(){
 	const User user;
 	std::string name("root");
+	Vector<Polymorphic_Ptr<BaseFile>> children;
+	Vector<std::string> groups;
+	groups.pushBack("Teachers");
+	Directory dir(name, user, nullptr, children, groups);
 	std::cout << dir;
 }
